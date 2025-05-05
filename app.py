a9aedfcbeeb05f49154b10b17e3c35588ecb6e7a
Ravisha Rupasinghe <rhrupasinghe@spartan-login1.hpc.unimelb.edu.au>
2025-05-05 13:19:11 +1000

    edit paragraph

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ st.title("Top DEE Gene Predictions")
 st.markdown(
     "This dashboard displays the top predicted DEE genes based on the "
     "latest model outputs. The displayed list of genes are obtained by training "
-    "the model for both AD and AR DEE genes. For the ranked list obtained for "
+    "the model for both AD and AR DEE genes. For the ranked list obtained by "
     "using only AR DEE and only AD DEE genes: [Only AR DEE](), [Only AD DEE]()"
 )
 
