42e60cee0d605ed377671ee522a6f1eb45de4303
Ravisha Rupasinghe <rhrupasinghe@spartan-login1.hpc.unimelb.edu.au>
2025-05-18 14:14:17 +1000

    fix bugs

--- pages/Gene_Explanation.py
+++ pages/Gene_Explanation.py
@@ -3,8 +3,8 @@ from urllib.parse import parse_qs
 import os
 
 # Get query parameters
-query_params = st.experimental_get_query_params()
-gene_name = query_params.get("gene", [None])[0]
+query_params = st.query_params
+gene_name = query_params.get("gene", None)
 
 if gene_name:
     #display_name = gene_name.replace('_', ' ')
@@ -15,13 +15,13 @@ if gene_name:
     if os.path.exists(image_path):
         #st.image(image_path, caption=f"Explanation for {display_name}")
         # --- Layout with two columns ---
-        col1, col2 = st.columns([3, 1])  # Wider graph, narrower legend
+        col1, col2 = st.columns([2, 1])  # Wider graph, narrower legend
 
         with col1:
             st.components.v1.html(open(f"images/{display_name}.html", "r").read(), height=800, scrolling=True)
 
         with col2:
-            st.image(image_path, caption="Edge Type Legend")
+            st.image(image_path)
     else:
         st.warning("No image found for this gene.")
 else:
